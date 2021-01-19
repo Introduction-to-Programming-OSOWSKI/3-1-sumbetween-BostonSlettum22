3986e217aa98c6cd76c621e47a7cd5e76f250179
BostonSlettum22 <816661@mystma.org>
2021-01-19 16:20:44 +0000

    sumbetween 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def sumBetween(x,y):
+    bazinga = 0
+    
+    for i in range(x + 5,y):
+        bazinga = bazinga + i
+
+    return bazinga
+
+
+print(sumBetween(2,13))
+
